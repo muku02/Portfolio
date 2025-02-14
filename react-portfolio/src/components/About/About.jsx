b60b1e12c276797ecb847c4c980e6e0ea0805867
mb <mbalaji2@binghamton.edu>
2025-02-14 13:45:40 -0800

    About section completed

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { getImageUrl } from '../../utils';
+import styles from './About.module.css';
 
 export const About = () => {
     return (
-        <section>
-            <h2>About</h2>
-            <div>
+        <section className={styles.container} id='about'>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
                 <img src={getImageUrl("/about/aboutImage.png")}  
                 alt='Me working'
+                className={styles.aboutImg}
                 />
-                <ul>
-                    <li>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("/about/cursorIcon.png")} 
                         alt='cursor Icon'
                         />
                         <h3>Software Developer</h3>
                         <p>
                             I'm a Software Developer with experience in building scalable and optimized applicatiions
+                        </p>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("/about/serverIcon.png")} 
+                        alt='server Icon'
+                        />
+                        <h3>Backend Developer</h3>
+                        <p>
+                            I'm a Backend Developer with experience in building fast and efficient APIs
+                        </p>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("/about/dbIcon.png")} 
+                        alt='DB Icon'
+                        />
+                        <h3>DB & Cloud Developer</h3>
+                        <p>
+                            I'm a Databse and Cloud Developer with experience in Database design, Development, optimization and effectively utilizing AWS cloud services like EC2, S3 and AWS bedrock
                         </p>
                     </li>
                 </ul>
